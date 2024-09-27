538550c0cbacd82799a633d2822dad1c95c4bf89
Linhcodelinhtinh <23020392@vnu.edu.vn>
2024-09-27 14:53:19 +0700

    nham

--- Chess/menu.py
+++ Chess/menu.py
@@ -4,14 +4,15 @@ import random
 import math
 import config as cg
 import warnings
+
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 p.init()
 
-AnKing = p.transform.rotate(p.transform.scale(p.image.load("Images/king.png"), (75, 75)),40)
-AnQueen = p.transform.rotate(p.transform.scale(p.image.load("Images/queen.png"), (75, 75)),40)
-AnPawn = p.transform.rotate(p.transform.scale(p.image.load("Images/pawn.png"), (75, 75)),40)
-AnKnight = p.transform.rotate(p.transform.scale(p.image.load("Images/knight.png"), (75, 75)),40)
+AnKing = p.transform.rotate(p.transform.scale(p.image.load("Images/king.png"), (75, 75)), 40)
+AnQueen = p.transform.rotate(p.transform.scale(p.image.load("Images/queen.png"), (75, 75)), 40)
+AnPawn = p.transform.rotate(p.transform.scale(p.image.load("Images/pawn.png"), (75, 75)), 40)
+AnKnight = p.transform.rotate(p.transform.scale(p.image.load("Images/knight.png"), (75, 75)), 40)
 clock = p.time.Clock()
 
 font = p.font.Font(None, 36)
@@ -22,12 +23,13 @@ gray = p.color.THECOLORS["grey44"]
 
 game_run = cg.game_run
 choice = cg.choice
-width = cg.width 
+width = cg.width
 height = cg.height
 dim = cg.dim
 p_size = width // dim
 FPS = cg.p_size
-screen = cg.screen 
+screen = cg.screen
+
 
 def draw_menu(choice, menu_run):
     screen.fill(p.Color(134, 165, 91))
@@ -48,12 +50,14 @@ def draw_menu(choice, menu_run):
 
     buttons = [
         {"text": "pvp", "rect": p.Rect(button_x, button_y, button_width, button_height)},
-        {"text": "AIEasy", "rect": p.Rect(button_x, button_y + button_height + button_spacing, button_width, button_height)},
-        {"text": "AIHard", "rect": p.Rect(button_x, button_y + 2 * button_height + 2 * button_spacing, button_width, button_height)}
+        {"text": "AIEasy",
+         "rect": p.Rect(button_x, button_y + button_height + button_spacing, button_width, button_height)},
+        {"text": "AIHard",
+         "rect": p.Rect(button_x, button_y + 2 * button_height + 2 * button_spacing, button_width, button_height)}
     ]
 
     for button in buttons:
-        p.draw.rect(screen, brown, button["rect"], border_radius=10) 
+        p.draw.rect(screen, brown, button["rect"], border_radius=10)
         button_text = font.render(button["text"], True, black)
         button_text_rect = button_text.get_rect(center=button["rect"].center)
         screen.blit(button_text, button_text_rect)
@@ -74,17 +78,25 @@ def draw_menu(choice, menu_run):
                     # Thêm mã để gọi file chế độ AIHard
     return choice, menu_run
 
+
 def random_pieces():
     pieces = [AnKing, AnQueen, AnPawn, AnKnight]
     x = random.randint(0, width - 20)
     y = -50
     random_piece = random.choices(pieces)[0]
-    falling_pieces = (random_piece, [x,y])
+    falling_pieces = (random_piece, [x, y])
     return falling_pieces
+
+
 falling_piece = random_pieces()
+falling_piece2 = random_pieces()
+
+
 def run(game_run):
     global falling_piece
+    # global falling_piece2
     falling_piece[1][1] += 1
+    count = 0
     clock.tick(FPS)
     for event in p.event.get():
         if event.type == p.QUIT:
@@ -92,7 +104,16 @@ def run(game_run):
     screen.fill(p.color.THECOLORS["chartreuse4"])
     choice, game_run = draw_menu(0, game_run)
     screen.blit(falling_piece[0], falling_piece[1])
-    if(falling_piece[1][1]>height):
+
+    if falling_piece[1][1] > height/2:
+        count = 1
+    if count == 1:
+        falling_piece2[1][1] += 1
+        screen.blit(falling_piece2[0], falling_piece2[1])
+    if falling_piece[1][1] > height:
         falling_piece = random_pieces()
+    if falling_piece2[1][1] > height:
+        falling_piece = random_pieces()
+
     p.display.flip()
     return game_run, choice
