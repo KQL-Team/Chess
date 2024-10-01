5269a8ab39706ab733e791964ac8f374ef9414b4
Linhcodelinhtinh <23020392@vnu.edu.vn>
2024-10-01 20:07:45 +0700

    commit

--- Chess/chess.py
+++ Chess/chess.py
@@ -266,7 +266,7 @@ class Game():
         
         
 def end_game(self):
-    if check_white:
+    if self.check_white():
         for x in range(8):
             for y in range(8):
                 if self.board[x][y][0] == "w":
@@ -274,7 +274,7 @@ def end_game(self):
                         for row in range(8):
                             if not self.restrict((x,y),(col,row)) and not self.move_leads_to_check:
                                 return False
-    if check_black:
+    if self.check_black():
         for x in range(8):
             for y in range(8):
                 if self.board[x][y][0] == "b":
