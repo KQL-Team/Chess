6a47be515e15d4a7c6d0100fd7a929fab06fb6b5
Khanhtoichoi <23020386@vnu.edu.vn>
2024-10-02 14:14:50 +0700

    Update controller.py

--- Chess/controller.py
+++ Chess/controller.py
@@ -6,12 +6,13 @@ game_run = cg.game_run
 check = True
 while game_run:
     if game_state == 0:
-        game_run, game_state = menu.run()
         if  not check:
             menu.reset()
         check = True
+        game_run, game_state = menu.run()
     else:
-        game_run, game_state = main.main()
         if check:
             main.reset()
             check = False
+        game_run, game_state = main.main()
+
