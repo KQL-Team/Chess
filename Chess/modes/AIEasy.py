737967af639312f04f6f14196b76bbe09aa29055
Khanhtoichoi <23020386@vnu.edu.vn>
2024-10-04 01:42:08 +0700

    Update AIEasy.py

--- Chess/modes/AIEasy.py
+++ Chess/modes/AIEasy.py
@@ -1,5 +1,3 @@
-import numpy as np
-import copy
 
 piece_values = {
     'P': 10,
@@ -11,9 +9,9 @@ piece_values = {
 }
 
 class AIEasy():
-    def __init__(self, game, depth=3):
+    def __init__(self, game):
         self.game = game
-        self.depth = depth
+        self.depth = 3
 
     def evaluate_board(self, board):
         evaluation = 0
@@ -28,17 +26,32 @@ class AIEasy():
                         evaluation -= value
         return evaluation
 
-    def alpha_beta(self, game, depth, alpha, beta, maximizing_player):
-        if depth == 0 or game.end_game()[0]:
-            return self.evaluate_board(game.board), None
-
+    def alpha_beta(self, depth, alpha, beta, maximizing_player):
+        if depth == 0 :
+            return self.evaluate_board(self.game.board), None
+        if self.game.end_game() == (True, 'lose'):
+            return self.evaluate_board(self.game.board) + 1000000, None
+        if self.game.end_game() == (True, 'win'):
+            return self.evaluate_board(self.game.board) - 1000000, None
+        if self.game.end_game() == (True, 'draw'):
+            return self.evaluate_board(self.game.board) - 500, None
         best_move = None
         if maximizing_player:
             max_eval = float('-inf')
-            for src, dest in self.get_all_moves(game, 'b'):
-                temp_game = copy.deepcopy(game)
-                temp_game.move(src, dest)
-                eval = self.alpha_beta(temp_game, depth - 1, alpha, beta, False)[0]
+            for src, dest in self.get_all_moves('b'):
+                temp_src = self.game.board[src[0]][src[1]]
+                temp_dest = self.game.board[dest[0]][dest[1]]
+                self.game.move(src, dest)
+                eval = self.alpha_beta(depth - 1, alpha, beta, False)[0]
+                self.game.board[dest[0]][dest[1]] = temp_dest
+                self.game.board[src[0]][src[1]] = temp_src
+                if temp_src == 'bK' and src == (0,4) and dest == (0,1):
+                    self.game.board[0][2] = ''
+                    self.game.board[0][0] = 'bR'
+                elif temp_src == 'bK' and src == (0,4) and dest == (0,6):
+                    self.game.board[0][5] = ''
+                    self.game.board[0][7] = 'bR'
+                self.game.ck[src[0]][src[1]] = 0
                 if eval > max_eval:
                     max_eval = eval
                     best_move = (src, dest)
@@ -48,10 +61,22 @@ class AIEasy():
             return max_eval, best_move
         else:
             min_eval = float('inf')
-            for src, dest in self.get_all_moves(game, 'w'):
-                temp_game = copy.deepcopy(game)
-                temp_game.move(src, dest)
-                eval = self.alpha_beta(temp_game, depth - 1, alpha, beta, True)[0]
+            for src, dest in self.get_all_moves('w'):
+                print(src, dest)
+                temp_src = self.game.board[src[0]][src[1]]
+                temp_dest = self.game.board[dest[0]][dest[1]]
+                self.game.move(src, dest)
+                eval = self.alpha_beta(depth - 1, alpha, beta, True)[0]
+                print(dest, src, "undo")
+                self.game.board[dest[0]][dest[1]] = temp_dest
+                self.game.board[src[0]][src[1]] = temp_src
+                if temp_src == 'wK' and src == (7,4) and dest == (7,6):
+                    self.game.board[7][5] = ''
+                    self.game.board[7][7] = 'wR'
+                elif temp_src == 'wK' and src == (7,4) and dest == (7,1):
+                    self.game.board[7][2] = ''
+                    self.game.board[7][0] = 'wR'
+                self.game.ck[src[0]][src[1]] = 0
                 if eval < min_eval:
                     min_eval = eval
                     best_move = (src, dest)
@@ -60,18 +85,18 @@ class AIEasy():
                     break
             return min_eval, best_move
 
-    def get_all_moves(self, game, player):
+    def get_all_moves(self, player):
         all_moves = []
         for x in range(8):
             for y in range(8):
-                piece = game.board[x][y]
+                piece = self.game.board[x][y]
                 if piece != '' and piece[0] == player:
                     for row in range(8):
                         for col in range(8):
-                            if not game.restrict((x, y), (row, col)) and not game.move_leads_to_check((x, y), (row, col)):
+                            if not self.game.restrict((x, y), (row, col)) and not self.game.move_leads_to_check((x, y), (row, col)):
                                 all_moves.append(((x, y), (row, col)))
         return all_moves
 
     def select_best_move(self):
-        _, best_move = self.alpha_beta(self.game, self.depth, float('-inf'), float('inf'), True)
+        _, best_move = self.alpha_beta(self.depth, float('-inf'), float('inf'), True)
         return best_move
